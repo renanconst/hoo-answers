c6d47834d4dee92f3f7657d3297a3f985cd4076a
Renan Lima <75452779+renanllima@users.noreply.github.com>
2021-08-04 19:05:48 -0300

    feat: add question/ask and question/reply pages

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Form } from './pages/Form';
 import { Landing } from './pages/Landing';
 import { Questions } from './pages/Questions';
 
 export const Routes = () => (
   <Switch>
     <Route path="/" component={Landing} exact />
-    <Route path="/questions" component={Questions} />
+    <Route path="/questions" component={Questions} exact />
+    <Route path="/questions/ask" component={Form} />
+    <Route path="/questions/reply" component={Form} />
   </Switch>
 );
